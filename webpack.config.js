ae9a0e08b54e40674e95deccd979a0c7880562c9
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-02 16:07:32 +0200

    Set default mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: './dist',
