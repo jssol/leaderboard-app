e268ae76cc2f84c34035ded729091b2d4bf41ecd
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-04 03:45:45 +0200

    Build the pre-release

--- dist/main.js
+++ dist/main.js
@@ -38,7 +38,27 @@ var __webpack_exports__ = {};
     });
   });
 });
+;// CONCATENATED MODULE: ./src/modules/darkMode.js
+/* harmony default export */ const darkMode = ((document, btn) => {
+  const light = 'light';
+  const dark = 'dark';
+  if (document.classList.contains(light)) {
+    document.classList.remove(light);
+    document.classList.add(dark);
+    btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />
+    </svg>`;
+  } else if (document.classList.contains(dark)) {
+    document.classList.remove(dark);
+    document.classList.add(light);
+    btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+    </svg>`;
+  }
+});
+
 ;// CONCATENATED MODULE: ./src/index.js
+
 
 
 
@@ -48,6 +68,7 @@ const refreshBtn = document.querySelector('.board-refresh-btn');
 const nameInput = document.getElementById('name');
 const scoreInput = document.getElementById('score');
 const scoreForm = document.querySelector('.score-form');
+const themeBtn = document.querySelector('.dark-toggle');
 
 const sudoku = new api('Sudoku');
 
@@ -68,5 +89,10 @@ scoreForm.onsubmit = (e) => {
     scoreForm.reset();
   }
 };
+
+themeBtn.addEventListener('click', (e) => {
+  darkMode(document.documentElement, e.target);
+});
+
 /******/ })()
 ;
